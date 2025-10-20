a2e29a0f902b62b155d9b0cc406c4e5191788549
yahya <yahyaozay1905@gmail.com>
2025-10-19 22:06:57 -0500

    correct variable name

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME
 });
 
