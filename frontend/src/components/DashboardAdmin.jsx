11c9e5bf0b71ba2252268c610fcbfba3d356f74d
yahya <yahyaozay1905@gmail.com>
2025-10-24 09:29:28 -0500

    update on admin dashboard

--- frontend/src/components/DashboardAdmin.jsx
+++ frontend/src/components/DashboardAdmin.jsx
@@ -2,7 +2,7 @@ export default function DashboardAdmin() {
     return (
       <div className="p-6 space-y-4">
         <h1 className="text-2xl font-semibold">Admin Dashboard</h1>
-        <p>Full control over Artists, Artworks, and Reports.</p>
+        <p>Ful control over Artists, Artworks, and Reports.</p>
       </div>
     );
   }
